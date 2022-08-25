04dd64c2f06807b73386d9a30dc4502765c078e6
Ferdinand Vasquez <killcmd@gmail.com>
2022-08-24 23:54:53 -0400

    Lawnchair: Removed.
    Incompatible with Android 13

--- cup/custom/custom.mk
+++ cup/custom/custom.mk
@@ -1,8 +1,2 @@
 # Preload My Boy
 #Add Packages to load with install.
-
-$(call inherit-product-if-exists, device/oneplus/instantnoodle/cup/custom/Lawnchair/lawnchair.mk)
-
-
-PRODUCT_PACKAGES += \
-Lawnicons
